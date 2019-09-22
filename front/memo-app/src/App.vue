438d39f86046fa6662b8d5035fbc3d6f38b63d31
minomi <5minhominho@gmail.com>
2019-09-22 21:25:51 +0900

    AppHeader 컴포넌트 생성

--- front/memo-app/src/App.vue
+++ front/memo-app/src/App.vue
@@ -1,45 +1,22 @@
 <template>
   <div id="app">
-
+    <app-header/>
   </div>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader"
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  components: {AppHeader},
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "./styles/reset.css";
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  #app {
+    width: 500px;
+    margin: 0 auto;
+  }
 </style>
